e9f49f585bd65823602f362258fb8a7da3ce1387
Aayush Chopra <aayushking37@gmail.com>
2023-10-02 15:22:53 +0530

    ff

---
+++ src/App.js
@@ -0,0 +1,202 @@
+import icon from './icons8-play-48.png';
+import previousimage from './previous.png';
+import pause from './pause.png';
+import next_img from './next.png';
+import logo from './icons8-music-64.png';
+import menu_logo from './hamburger.png';
+import close_logo from './icons8-close-window-50.png';
+import './App.css';
+import './Devices.css';
+import track from './tracks';
+import React,{useState,useRef} from 'react';
+
+function App() {
+  let [currTrack,setTrack] = useState(0);
+  // let sound = new Audio(track[currTrack].src);
+  const audioRef = useRef(new Audio(track[currTrack].src));
+  const sound = audioRef.current;
+
+  const playing = () => {
+    document.getElementById('play').style.display='none';
+    document.getElementById("pause").style.display="block";
+    console.log(sound);
+    sound.play();
+    sound.ontimeupdate = function() {
+      document.getElementById('progress').style.width = Math.floor(100*sound.currentTime/sound.duration)+"%";
+      document.getElementById('startTime').innerHTML = formatTime(sound.currentTime);
+      document.getElementById('endTime').innerHTML = formatTime(sound.duration);
+    };
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? '0':''}${seconds}`;
+  }
+
+  const pausing = () => {
+    document.getElementById('play').style.display='block' ;
+    document.getElementById("pause").style.display="none";
+    sound.pause();
+  }
+
+  const next = () => {
+    document.getElementById('play').style.display='block';
+    document.getElementById("pause").style.display="none";
+    currTrack+=1;
+    setTrack(currTrack);
+    sound.src = track[currTrack].src;
+    console.log(sound.src);
+    sound.pause();
+  }
+
+  const previous = () => {
+    document.getElementById('play').style.display='block' ;
+    document.getElementById("pause").style.display="none";
+    currTrack-=1;
+    setTrack(currTrack);
+    sound.src = track[currTrack].src;
+    sound.pause();
+  }
+  const bar = (e) => {
+    console.log(e);
+    console.log(e.nativeEvent.offsetX*100 / document.getElementById('progressed_bar').offsetWidth * sound.duration);
+    sound.currentTime = (e.nativeEvent.offsetX / document.getElementById('progressed_bar').offsetWidth) * sound.duration;
+  }
+
+  const musicPlay = (indexVal) => {
+    currTrack = indexVal;
+    setTrack(currTrack);
+    sound.src = track[currTrack].src;
+    playing();
+  }
+
+  const open_menu = () => {
+    const x = document.getElementById("pop-menu");
+    x.style.display = 'block';
+
+    const y = document.getElementById("menu");
+    y.style.display = 'none';
+
+    const z = document.getElementById("close");
+    z.style.display = 'flex';
+  }
+
+  const close_menu = () => {
+    const x = document.getElementById("close");
+    x.style.display = 'none';
+
+    const y = document.getElementById("menu");
+    y.style.display = 'flex';
+
+    const z = document.getElementById("pop-menu");
+    z.style.display = 'none';
+  
+  }
+  return (
+      <>
+        <div id="section1">
+        <nav className="navbar">
+            <header className="head">
+                <div className="left">
+                  <span style={{width:'fit-content',margin:'auto 5px'}}><img src={logo} alt='' className='logo_img'/></span>
+                  <h2>Music Tech</h2>
+                </div>
+                <div className="right">
+                    <ul className="opts">
+                      <li>Podcasts</li>
+                      <li>Categories</li>
+                      <li>About Us</li>
+                      <li>Support</li>
+                    </ul>
+                    <ul id='all_menu_btn'>
+                      <li onClick={open_menu} id="menu"><img src={menu_logo} alt=''/></li>
+                    </ul>
+                </div>
+            </header>
+        </nav>
+      </div>
+
+      <div id='displaying'>
+        <div className='music_list'>
+          <div style={{display: 'block',width: '100%',padding: '10px'}}>
+            <div className='common'>
+              <h4 className='heading_style'>{track[0].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(0)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[1].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(1)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[2].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(2)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[3].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(3)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[4].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(4)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[5].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(5)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[6].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(6)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[7].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(7)} />
+            </div>
+            <div className='common'>
+              <h4 className='heading_style'>{track[8].title}</h4>
+              <img src={icon} alt='icon' id='first' onClick={()=>musicPlay(8)} />
+            </div>
+          </div>
+        </div>
+        <div className='right_player'>
+          <div style={{width:'fit-content',margin:'auto'}}>
+            <div style={{width:'fit-content', margin:'1rem auto', backgroundColor:'transparent'}}>
+              <img src={track[currTrack].thumbnail} alt='passori song' className='song' id='image'/>
+            </div>
+            <div style={{width:'100%'}}>
+              <h2 id='title' style={{textAlign: 'center', fontSize:'35px', color:'white'}}>{track[currTrack].title}</h2>
+              <h4 id='author' style={{textAlign: 'center', fontSize:'20px',color:'white'}}>{track[currTrack].Author}</h4>
+            </div>
+            <div style={{display:'flex',justifyContent:'space-between',color:'white'}}>
+              <span id='startTime'>0:00</span>
+              <span id='endTime'>0:00</span>
+            </div>
+            <div style={{width:'100%',height:'10px',backgroundColor:'white',borderRadius:'100px'}} id='progressed_bar' onClick={bar}>
+              <div style={{width:'0%',height:'10px',backgroundColor:'#404373',borderRadius:'100px'}} id='progress'></div>
+            </div>
+            <div style={{display:'flex',justifyContent:'space-between',margin:'1rem auto'}}>
+              <img src={previousimage} alt='previous button' style={{backgroundColor:'white',borderRadius:'10px',cursor:'pointer '}} onClick={previous}/>
+              <img src={icon} alt='icon' style={{backgroundColor:'white',borderRadius:'10px',cursor:'pointer '}} id='play' onClick={playing}/>
+              <img src={pause} alt='pause button' style={{backgroundColor:'white',borderRadius:'10px',cursor:'pointer',display:'none'}} id='pause'onClick={pausing} />
+              <img src={next_img} alt='next button' style={{backgroundColor:'white',borderRadius:'10px',cursor:'pointer'}} onClick={next}/>
+            </div>
+          </div>
+        </div>
+
+        <div id="pop-menu">
+          <div id="close">
+            <img src={close_logo} onClick={close_menu} alt=''/>
+          </div>
+          <ul>
+            <li>Podcasts</li>
+            <li>Categories</li>
+            <li>About Us</li>
+            <li>Support</li>
+          </ul>
+        </div>
+      </div>
+      </>
+  );
+}
+
+export default App;
---
+++ src/tracks.js
@@ -0,0 +1,77 @@
+import pasoori from './music/pasoori.mp3';
+import t1 from './pasoori song.png';
+import birthdaygift from './music/Birthday Gift.mp3';
+import t2 from './birthday gift.jpg';
+import dhakka from './music/Dhakka.mp3';
+import t3 from './dhakka.jpg';
+import dopeshope from './music/Dope Shope.mp3';
+import t4 from './dopeshope.jpg';
+import gallgoriye from './music/Gall Goriye.mp3';
+import t5 from './gallgoriye.jpg';
+import aj from './music/Athri Jawani.mp3';
+import t6 from './aj.jpg';
+import gk from './music/Gal Karke.mp3';
+import t7 from './gk.jpg';
+import cwb from './music/Choorhey Wali Bahh.mp3';
+import t8 from './cwb.jpg';
+import ho from './music/Haaye Oye.mp3';
+import t9 from './haayeoye.jpg';
+
+const track = [
+    {
+        title: 'Pasoori',
+        src: pasoori,
+        Author: 'Ali Sethi and debutant Shae Gill',
+        thumbnail: t1
+    },
+    {
+        title: 'Birthday Gift',
+        src: birthdaygift,
+        Author:'Sharry Mann',
+        thumbnail: t2
+    },
+    {
+        title: 'Dhakka',
+        src: dhakka,
+        Author: 'Siddhu Moosewala',
+        thumbnail:t3
+    },
+    {
+        title: 'Dope Shope',
+        src: dopeshope,
+        Author: 'Deep Money and Honey Singh',
+        thumbnail: t4
+    },
+    {
+        title: 'Gall Goriye',
+        src: gallgoriye,
+        Author: 'Raftaar and Manindar Buttar',
+        thumbnail: t5
+    },
+    {
+        title: 'Athri Jawani',
+        src :aj,
+        Author: 'Ammy Virk and Gurlez Akhtar',
+        thumbnail: t6
+    },
+    {
+        title: 'Gal Karke',
+        src: gk,
+        Author: 'Inder Chahal',
+        thumbnail: t7
+    },
+    {
+        title: 'Choorey Waali Bahh',
+        src: cwb,
+        Author: 'Mankrit Aulakh',
+        thumbnail: t8
+    },
+    {
+        title: 'Haaye Oye',
+        src: ho,
+        Author: 'Qaran',
+        thumbnail: t9
+    }
+]
+
+export default track;
